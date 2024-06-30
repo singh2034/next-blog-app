fc8e263eb1383a762163a950760528dfd5b9da83
Aayush <singhaayush2034@gmail.com>
2024-06-30 21:16:59 +0530

    updated

---
+++ app/api/blog/route.js
@@ -0,0 +1,54 @@
+import { connectDB } from "@/lib/config/db";
+import { NextResponse } from "next/server";
+import { writeFile } from "fs/promises";
+import BlogModel from "@/lib/models/BlogModel";
+
+const LoadDB = async () => {
+  await connectDB();
+};
+
+LoadDB();
+
+// GET Blog
+export async function GET(request) {
+  return NextResponse.json({ msg: "API Working" });
+}
+
+// POST || CREATE Blog
+export async function POST(request) {
+  try {
+    const formData = await request.formData();
+    const timestamp = Date.now();
+    const image = formData.get("image");
+    if (!image) {
+      return NextResponse.json(
+        { success: false, msg: "Image is required" },
+        { status: 400 }
+      );
+    }
+    const imageByteData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByteData);
+    const path = `./public/${timestamp}_${image.name}`;
+    await writeFile(path, buffer);
+    const imgUrl = `/${timestamp}_${image.name}`;
+
+    const blogData = {
+      title: `${formData.get("title")}`,
+      description: `${formData.get("description")}`,
+      category: `${formData.get("category")}`,
+      author: `${formData.get("author")}`,
+      img: `${imgUrl}`,
+      authorImg: `${formData.get("authorImg")}`,
+    };
+
+    await BlogModel.create(blogData);
+    console.log("Blog Saved");
+    return NextResponse.json({ success: true, msg: "Blog Added" });
+  } catch (error) {
+    console.log("error:", error);
+    return NextResponse.json(
+      { succes: false, msg: "Server Error" },
+      { status: 500 }
+    );
+  }
+}
